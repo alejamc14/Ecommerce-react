0f99feb383191a36ea6aa4a465bf9a6aff1c95bf
alejamc14 <alejandra.madrid1426@gmail.com>
2026-04-25 16:34:37 -0500

    Fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import { Login } from "./pages/Login";
-import {Products}  from "./pages/Products";
-import {ProductDetail}  from "./pages/ProductDetail";
-
+import Login from "./pages/Login.jsx";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return(
     <Routes>
       <Route path="/" element={<Login/>} />
-      <Route path="/products" element={<Products/>}>
-        <Route path="/:id" element={<ProductDetail/>}/>
-      </Route>
+      <Route path="/products" element={<Products/>}/>
+      <Route path="/products/:id" element={<ProductDetail/>}/>
     </Routes>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,4 +2,14 @@ import { Navigate } from "react-router-dom";
 
 export default function Login(){
 
+    const handleSubmit = () => {
+       return <Navigate to="/products" replace/>
+    }
+
+    return(
+        <div>
+            <h1>Login</h1>
+            <button onClick={handleSubmit}>Login</button>
+        </div>
+    );
 }
